95de053658742b69b65136aa441cd9fd7dd5648d
Mayner <meiningzhu@163.com>
2018-05-24 23:30:03 +0800

    add some test rules

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const add = (a, b) => {
-    return a + b;
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+
+    return NaN;
 };
 
 module.exports = { add  };
+  
